74b121ddf189b7000cd7fe705fd7c0445723e7ed
deadlyblue <deadlyblue1@yandex.ru>
2024-03-16 18:38:13 +0300

    first commit

---
+++ lib/modul/DataBase.dart
@@ -0,0 +1,87 @@
+class Product {
+  String phoneNames;
+  String phoneDescriptions;
+  String phoneImages;
+  String price;
+
+  Product(
+      this.phoneNames, this.phoneDescriptions, this.phoneImages, this.price);
+}
+
+List<Product> productList = [
+  Product(
+      "Холодный зелёный чай Lipton 1л",
+      "Холодный зелёный чай. Освежает и утоляет жажду. Ещё вкуснее, если пить охлаждённым. ",
+      'https://cm.samokat.ru/processed/l/product_card/1f021f31-54fa-4c2b-9a62-bc377e506156.jpg',
+      "159 ₽"),
+  Product(
+      "Adrenaline Rush ",
+      "Безалкогольный энергетик. Содержит кофеин и витамины группы B. Бодрит и тонизирует. Ещё вкуснее, если пить охлаждённым.",
+      'https://cm.samokat.ru/processed/l/product_card/58d1c829-7966-48fe-8201-b214dba5caea.jpg',
+      "125 ₽"),
+  Product(
+      "Тоник Evervess Итальянский аперо, с газом ",
+      "Сильногазированный безалкогольный напиток. Имеет пряный апельсиновый вкус с горчинкой. Можно пить просто так или использовать для приготовления коктейлей.",
+      'https://cm.samokat.ru/processed/l/public/433954bc3dc6796a_4600494662430.jpg',
+      "166 ₽"),
+  Product(
+      "Dr. Pepper Original",
+      "Любимый напиток Вани Лиуфы",
+      'https://cm.samokat.ru/processed/l/product_card/6d8f2433-a002-4887-b9f6-dac46b3a4325.jpg',
+      "119 ₽"),
+  Product(
+      "Напиток Chupa Chups, дыня и сливки, с газом ",
+      "Не любимый напиток Вани Лиуфы",
+      'https://cm.samokat.ru/processed/l/public/634c2208828d46ea_8801069412979.jpg',
+      "109 ₽"),
+  Product(
+      "Напиток Добрый, апельсин, с газом ",
+      "Сильногазированный напиток со вкусом апельсина. Ещё вкуснее, если пить охлаждённым. ",
+      'https://cm.samokat.ru/processed/l/product_card/d1204fb8-5b0f-4591-bf63-b33792886463.jpg',
+      "109 ₽"),
+  Product(
+      "Напиток Добрый, лимон и лайм, с газом ",
+      "Сильногазированный напиток со вкусом лимона и лайма. Ещё вкуснее, если пить охлаждённым.",
+      'https://cm.samokat.ru/processed/l/product_card/9ddf11a8-b58c-4a84-b831-bf4d521b802a.jpg',
+      "109 ₽"),
+  Product(
+      "Напиток Frustyle, лимон и лайм, с газом ",
+      "Сильногазированный напиток со вкусом лимона и лайма. Ещё вкуснее, если пить охлаждённым.",
+      'https://cm.samokat.ru/processed/l/public/5fff99e70b474a3e_4600494685262.jpg',
+      "139 ₽"),
+  Product(
+      "Кола Evervess Zero Sugar, с газом ",
+      "Сильногазированный напиток со вкусом колы без добавления сахара. Можно пить просто так или использовать для приготовления коктейлей. Попробуйте охлаждённым.",
+      'https://cm.samokat.ru/processed/l/public/cc3643875021eb25_4600494687273.jpg',
+      "99 ₽"),
+  Product(
+      "Кола Любимая, с газом ",
+      "Сильногазированный напиток со вкусом колы и сладкими нотками ванили и корицы. Ещё вкуснее, если пить охлаждённым. ",
+      'https://cm.samokat.ru/processed/l/product_card/995720d2-7dd8-421e-9cd6-5d1cbe317af2.jpg',
+      "129 ₽"),
+  Product(
+      "Напиток CoolCola, с газом",
+      "Сильногазированный напиток со вкусом колы.",
+      'https://cm.samokat.ru/processed/l/product_card/d422eb68-5782-4655-85eb-8a10ebc94ed6.jpg',
+      "135 ₽"),
+  Product(
+      "Кола Добрый, с газом ",
+      "Классическая сильногазированная кола. Ещё вкуснее, если пить охлаждённой.",
+      'https://cm.samokat.ru/processed/l/product_card/03c881c4-f84c-48cf-8259-f7ae7c98e26e.jpg',
+      "109 ₽"),
+  Product(
+      "Кола Добрый, без сахара, с газом ",
+      "Классическая сильногазированная кола без сахара. Ещё вкуснее, если пить охлаждённой.",
+      'https://cm.samokat.ru/processed/l/product_card/5fa3fdc5-560f-4bd7-9b7a-9e6256b31441.jpg',
+      "109 ₽"),
+  Product(
+      "Coca-Cola",
+      "Вкус настояший",
+      'https://cm.samokat.ru/processed/l/public/feca3dfae37730e9_5449000054227.jpg',
+      "189 ₽"),
+  Product(
+      "Из Грузии: Лимонад Zandukeli, саперави, с газом ",
+      "Газированный напиток тёмно-рубиного цвета с ароматом чёрного винограда Саперави. Изготовлен на основе воды из горных источников Грузии. Ещё вкуснее, если пить охлаждённым.",
+      'https://cm.samokat.ru/processed/l/product_card/744800a7-8c62-4b16-8492-f7e715f6a835.jpg',
+      "119 ₽"),
+];
